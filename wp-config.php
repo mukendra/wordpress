98fcd6d4c07fe20ac0a3021ad71aa870cf02068f
mukendra <mukendra@users.noreply.github.com>
2017-01-28 20:14:31 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('DB_USER', 'wp');
 /** MySQL database password */
 define('DB_PASSWORD', 'root');
 /** MySQL hostname */
-define('DB_HOST', '192.168.1.63');
+define('DB_HOST', '192.168.1.232');
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
